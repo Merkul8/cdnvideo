6e37d148036e572b33d4a9fe6bd0e7a135d8d8ad
Merkul8 <max.merkulov.00@mail.ru>
2024-01-25 19:33:46 +0300

    create City model

--- map/models.py
+++ map/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Город')
+    latitude = models.FloatField(verbose_name='Широта')
+    longitude = models.FloatField(verbose_name='Долгота')
+
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+    
